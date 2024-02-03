a5f580284e67db0dd9c8fdf17175667b3bece58a
Rajin Gangadharan <rajin.gangadharan@gmail.com>
2024-02-03 08:58:04 +0530

    Second Part

---
+++ code .c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    printf("\nWelcome to Glug Meet!!!\n");
+    printf("Streamlining CI/CD pipeline using Jenkins and Git\n");
+
+    printf("\nSecond Part\n");
+    printf("Streamlining CI/CD pipeline using Jenkins and Git\n");
+    return 0;
+}
